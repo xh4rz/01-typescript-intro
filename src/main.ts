41eb43a0ae1c3ab8fc63c2007c6616aec6e3d01b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-07 15:14:35 -0500

    22. Desestructuración de argumentos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { setupCounter } from './counter.ts';
 // import './topics/02-object-interface';
 // import './topics/03-functions';
 // import './topics/04-homework-types';
-import './topics/05-basic-destructuring';
+// import './topics/05-basic-destructuring';
+import './topics/06-function-destructuring';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  Hola Mundo
---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,40 @@
+interface Product {
+	description: string;
+	price: number;
+}
+
+const phone: Product = {
+	description: 'Nokia A1',
+	price: 150.0
+};
+
+const tablet: Product = {
+	description: 'iPad Air',
+	price: 250.0
+};
+
+interface TaxCalculationOptions {
+	tax: number;
+	products: Product[];
+}
+
+function taxCalculation({ products, tax }: TaxCalculationOptions): number[] {
+	let total = 0;
+
+	products.forEach(({ price }) => {
+		total += price;
+	});
+
+	return [total, total * tax];
+}
+
+const shoppingCart = [phone, tablet];
+
+const tax = 0.15;
+
+const [total, impuestos] = taxCalculation({ products: shoppingCart, tax: tax });
+
+console.log('Total', total);
+console.log('Total', impuestos);
+
+export {};
