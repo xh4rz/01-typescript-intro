023ba47b6996f0dd80fc4cdde5e6ae3c950b9115
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-08 00:59:11 -0500

    29. Genéricos

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import { setupCounter } from './counter.ts';
 // import './topics/05-basic-destructuring';
 // import './topics/06-function-destructuring';
 // import './topics/07-import-export';
-import './topics/08-classes';
+// import './topics/08-classes';
+import './topics/09-generics';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  Hola Mundo
