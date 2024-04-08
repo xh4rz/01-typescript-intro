e81a9df0e8bdd205fbf4ba84c2dd93f486c12c59
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-08 00:46:34 -0500

    28. Priorizar composición sobre herencia

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -2,19 +2,38 @@ export class Person {
 	// public name: string;
 	// private address: string;
 
-	constructor(public name: string, private address: string = 'No Address') {}
+	constructor(
+		public firstName: string,
+		public lastName: string,
+		private address: string = 'No Address'
+	) {}
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+// 	constructor(
+// 		public alterEgo: string,
+// 		public age: number,
+// 		public realName: string
+// 	) {
+// 		super(realName, 'New York');
+// 	}
+// }
+
+export class Hero {
+	// public person: Person;
+
 	constructor(
 		public alterEgo: string,
 		public age: number,
-		public realName: string
+		public realName: string,
+		public person: Person
 	) {
-		super(realName, 'New York');
+		// this.person = new Person(realName);
 	}
 }
 
-const ironman = new Hero('Ironman', 45, 'Tony');
+const tony = new Person('Tony', 'Stark', 'New York');
+
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
 
 console.log(ironman);
